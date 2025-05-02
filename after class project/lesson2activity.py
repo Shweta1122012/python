cad21a41644bff21a11e60b63cd91cf897011307
Shweta1122012 <vishal.vings@gmail.com>
2025-05-02 16:08:41 +0530

    move to github

---
+++ after class project/lesson2activity.py
@@ -0,0 +1,10 @@
+saloni= 30
+rahul =24
+shourya =15
+siya =16
+shweta=11
+print ("raj's friend saloni birthdate is on:",saloni)
+print ("raj's friend rahul birthdate is on:",rahul)
+print ("raj's friend shourya birthdate is on:",shourya)
+print ("raj's friend siya birthdate is on:",siya)
+print ("raj's friend shweta birthdate is on:",shweta)
