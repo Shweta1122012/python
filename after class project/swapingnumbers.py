2dfc69ded2e621b29b762ce402100142914b856d
Shweta1122012 <vishal.vings@gmail.com>
2025-05-16 15:42:41 +0530

    after class project

---
+++ after class project/swapingnumbers.py
@@ -0,0 +1,8 @@
+a=int(input("enter the first number (a):"))
+b=int(input("enter the second number (b):"))
+c=int(input("enter the third number (c):"))
+print("\nbefore swapping:")
+print("a=",a,"b=",b,"c=",c)
+a,b,c=c,a,b
+print("\nafter swapping:")
+print("a=",a,"b=",b,"c=",c)
