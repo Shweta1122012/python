a20d9e405f8dec6a1c74419b46a8c902d3ce5ec6
Shweta1122012 <vishal.vings@gmail.com>
2025-05-24 15:26:58 +0530

    after class project

---
+++ after class project/checkingage.py
@@ -0,0 +1,9 @@
+n=int(input("enter your age:"))
+if n<10:
+    print("below the 10 to 20 years age reqirement")
+    if n>20:
+        print ("above the 10 to 20 years age requirment")
+else:
+    print ("is between 10 to 20 age requirment")
+    print ("is allowed")
+    
