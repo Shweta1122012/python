ac28cb8e02e949f6f5f2afdfbf38e16ee555f46e
Shweta1122012 <vishal.vings@gmail.com>
2025-06-14 11:15:38 +0530

    after class project

---
+++ after class project/circumfrance.py
@@ -0,0 +1,7 @@
+radius = int(input("enter the radius of the circle to calculate its circumference:"))
+def circumference(radius):
+    pie=3.14
+    return 2*pie*radius
+circumferenceValue = circumference(radius)
+print("the circumference of the circle is-->",circumferenceValue)
+    
