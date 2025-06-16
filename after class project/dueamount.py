3c2306ab7866d340afc1975c1799ad6c04675ae8
Shweta1122012 <vishal.vings@gmail.com>
2025-06-16 15:31:13 +0530

    after class project

--- after class project/dueamount.py
+++ after class project/dueamount.py
@@ -1,5 +1,5 @@
 bill = int(input("enter the bill amount"))
 payed = int(input("enter the amount you have paid"))
-def due(bill, payed):
+def dueamount(bill, payed):
     return bill - payed
 print("You still have to pay ", due(bill, payed))
