34aaf86cd1196526049c4af37d8b704c316c2487
Shweta1122012 <vishal.vings@gmail.com>
2025-07-02 07:23:13 -0400

    after class  project

---
+++ after class project/month.py
@@ -0,0 +1,52 @@
+import calendar
+month = int(input("enter the month in number (1-12)-->"))
+if month == 1:
+    yy=2025
+    mm=1
+    print(calendar.month(yy,mm))
+elif month == 2:
+    yy=2025
+    mm=2
+    print(calendar.month(yy,mm))
+elif month == 3:
+    yy=2025
+    mm=3
+    print(calendar.month(yy,mm))
+elif month == 4:
+    yy=2025
+    mm=4
+    print(calendar.month(yy,mm))
+elif month == 5:
+    yy=2025
+    mm=5
+    print(calendar.month(yy,mm))
+elif month == 6:
+    yy=2025
+    mm=6
+    print(calendar.month(yy,mm))
+elif month == 7:
+    yy=2025
+    mm=7
+    print(calendar.month(yy,mm))
+elif month == 8:
+    yy=2025
+    mm=8
+    print(calendar.month(yy,mm))
+elif month == 9:
+    yy=2025
+    mm=9
+    print(calendar.month(yy,mm))
+elif month == 10:
+    yy=2025
+    mm=10
+    print(calendar.month(yy,mm))
+elif month == 11:
+    yy=2025
+    mm=11
+    print(calendar.month(yy,mm))
+elif month == 12:
+    yy=2025
+    mm=12
+    print(calendar.month(yy,mm)) 
+else:
+    print ("invalid month please enter a number between 1 and 12")   
