4421ab3c75350c7ba4115e779f150115089c8eb8
Shweta1122012 <vishal.vings@gmail.com>
2025-07-19 05:53:33 -0400

    after class project

--- after class project/frequency.py
+++ after class project/frequency.py
@@ -1,1 +1,8 @@
 test_dict ={"codingal": 3, "is": 2, "best": 2, "for": 2, "coding": 1}
+print("The orignal dictionary is:",test_dict)
+k=int(input("enter the frequency to check"))
+res=0
+for key in test_dict:
+    if test_dict[key]==k:
+        res+=1
+print ("yhe number of times the given number is reapeting is:",res)
