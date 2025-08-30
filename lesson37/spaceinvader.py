166a8d29c21c48f2f9ba1bb5534750cae03389d1
Shweta1122012 <vishal.vings@gmail.com>
2025-08-30 05:49:07 -0400

    lesson37

---
+++ lesson37/spaceinvader.py
@@ -0,0 +1,71 @@
+import math
+import random
+import pygame
+
+SCREEN_WIDTH=800
+SCREEN_HEIGHT=500
+PLAYER_START_X=370
+PLAYER_START_Y=380
+ENEMY_START_Y_MIN=50
+ENEMY_START_Y_MAX=150
+ENEMY_SPEED_X=4
+ENEMY_SPEED_Y=40
+BULLET_SPEED_Y=10
+COLLISION_DISTANCE=27
+pygame.init()
+screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption("space invader")
+icon=pygame.image.load(r"C:\Users\Public\python\lesson37\ufo.png")
+pygame.display.set_icon(icon)
+
+playerimg=pygame.image.load(r"C:\Users\Public\python\lesson37\player.png")
+playerx=PLAYER_START_X
+playery=PLAYER_START_Y
+playerx_change=0
+
+enemyimg=[]
+enemyx=[]
+enemyy=[]
+enemyx_change=[]
+enemyy_change=[]
+num_of_enemy=6
+
+for _i in range(num_of_enemy):
+    enemyimg.append(pygame.image.load(r"C:\Users\Public\python\lesson37\enemy.png"))
+    enemyx.append(random.randint(0,SCREEN_WIDTH-64))
+    enemyy.append(random.randint(ENEMY_START_Y_MIN,ENEMY_START_Y_MAX))
+    enemyx_change.append(ENEMY_SPEED_X)
+    enemyy_change.append(ENEMY_SPEED_Y)
+
+bulletimg=pygame.image.load(r"C:\Users\Public\python\lesson37\bullet.png")
+bulletx=0
+bullety=PLAYER_START_Y
+bulletx_change=0
+bullety_change=BULLET_SPEED_Y
+bullet_state="ready"
+
+score_value=0
+font=pygame.font.Font("arial",32)
+textx=10
+texty=10
+over_font=pygame.font.Font("arial",64)
+
+def show_score(x,y):
+    score=font.render("score:"+str(score_value),True,(255,255,255))
+    screen.blit(score,(x,y))
+def game_over():
+    over_text=over_font.render("GAME OVER",True,(255,255,255))
+    screen.blit(over_text,(200,250))
+def player(x,y):
+    screen.blit(playerimg,(x,y))
+def enemy(x,y,i):
+    screen.blit(enemyimg[i],(x,y))
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state="fire"
+    screen.blit(bulletimg,(x+16,y+10))
+def is_collision(enemyx,enemyy,bulletx,bullety):
+    distance=math.sqrt((enemyx-bulletx)**2+(enemyy-bullety)**2)
+    return distance<COLLISION_DISTANCE
+        
+    
